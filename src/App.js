3809571c9849fe5636b173070d4b04f1a10ba851
Bhoowan <bhoowankapoor.bk@gmail.com>
2023-12-04 11:02:00 +0200

    Submitted week8-lab4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import Toolbar from './Toolbar';
+import Button from './Button';
+import Toolbar2 from './Toolbar2';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Reading Props in Event Handlers</h1>
+      <Toolbar />
+      <h1>React Events Lab</h1>
+      <Button />
+      <h1>Passing Event Handlers as Props</h1>
+      <Toolbar2 />
+      
     </div>
   );
 }
 
 export default App;
+
